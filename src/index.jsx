acde6e92b8f93e473cf9c8b745c595592692c281
Roman Nurik <roman@nurik.net>
2022-12-05 01:51:57 +0000

    Simplify vite and try github action

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./app.jsx";
-import { HelmetProvider } from "react-helmet-async";
 
-/**
- * Root of react site
- *
- * Imports Helment provider for the page head
- * And App which defines the content and navigation
- */
-
-// Render the site https://reactjs.org/docs/react-dom.html#render
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build"
-  },
-  server: {
-    strictPort: true,
-    hmr: {
-      port: 443 // Run the websocket server on the SSL port
-    }
-  }
+  plugins: [react()]
 });
